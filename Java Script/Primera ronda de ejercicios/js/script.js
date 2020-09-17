b7e36b534bd137edbcd26fd0833e27ca067fa285
meiphi <malena.cadima@gmail.com>
2020-09-17 17:33:24 -0300

    Inicio con javascript en el curso de Codo a Codo

---
+++ Java Script/Primera ronda de ejercicios/js/script.js
@@ -0,0 +1,14 @@
+document = {
+	nombre : "Malena"
+	usuario : "Meiphi"
+	mail : "malena.cadima@gmail.com" 
+}
+
+// TIPOS DE DATOS PRIMITIVOS 
+/*
+	String = Cadenas de texto.
+	Numbers = Valores numericos.
+	Boolean =  Verdadero / Falso.
+	Null = Contiene un valor explicitamente vacio.
+	Undefined = Contiene un valor vacio sin definir.
+*/
