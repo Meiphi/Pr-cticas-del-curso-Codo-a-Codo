a7e130659d182f848d2373e158de344964bf6ca9
malenacadima <malena.cadima@gmail.com>
2020-11-08 11:44:14 -0300

    actualizo

---
+++ Java_Script_DOM/practica/script.js
@@ -0,0 +1,8 @@
+let jugada = function() {
+	let celda = document.getElementsByTagName("button");
+/*	celda.style.background = '#F04';*/
+/*	celda[0].className = "celda rojo"*/
+	console.log('celda')
+}
+
+celda.className = celda.className.replace("verde");
---
+++ lista_de_tareas/script.js
@@ -0,0 +1,16 @@
+(function () {
+	//Variables
+	let lista = document.getElementById("lista");
+	let tareaInput = document.getElementById("tareaInput");
+	let btnNuevaTarea = document.getElementById("btn-agregar");
+
+	//Funciones
+	let agregarTarea = function () {
+		/* body... */
+	};
+
+	//Eventos
+	// Agregar tarea
+	btnNuevaTarea.addEventListener("click", agregarTarea);
+	// Comprobar input
+}());
